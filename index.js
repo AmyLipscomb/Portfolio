9893235ca6cf32f912742ba716d7bfb2e24c5441
amylipscomb <>
2022-10-31 22:57:38 -0400

    updated the css in style2.css, reworded the main page in index.html

---
+++ index.js
@@ -0,0 +1,31 @@
+// const portfolioSection = `${portfolioSection.aboutMe}, ${portfolioSection.work}`;
+
+// console.log(portfolioSection);
+
+// const page = `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>${portfolioSection.aboutMe}</title>
+
+//        <!-- To link in Bootstrap -->
+//     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" /> 
+//      <!-- To link in 'Google Font' -->
+//      <link href="https://fonts.googleapis.com/css2?family=Bungee+Shade&family=Great+Vibes&family=Poiret+One&family=Roboto:wght@100&display=swap" rel="stylesheet">
+
+
+//        <!-- To link in personalized style.css-->
+//     <link rel="stylesheet" href="assets/css/style2.css"/>
+// </head>
+
+//     <body>
+//         <h1>${portfolioSection.aboutMe}</h1>
+//         <h2>${portfolioSection.work}</h2>
+//     </body>
+// </html>
+
+// `
+
+//Remember to LINK THIS TO MY INDEX.HTML FILE!!!
